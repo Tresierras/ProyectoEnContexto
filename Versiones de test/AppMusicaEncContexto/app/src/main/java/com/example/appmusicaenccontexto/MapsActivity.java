3e563798ad1b82cb050055e4dc2cb8047894f648
Fer3sierrasDev <Fer3sierrasDev@users.noreply.github.com>
2019-09-26 21:27:45 -0700

    Test Acelerometro
    La funcion de acelerometro esta incluida con una sola cancion, al detectar movimiento de derecha a izquierda este reproduce la cancion, pero, si el movimiento es constante, se reproduce la cancion en un bucle infinito.

---
+++ Versiones de test/AppMusicaEncContexto/app/src/main/java/com/example/appmusicaenccontexto/MapsActivity.java
@@ -0,0 +1,80 @@
+package com.example.appmusicaenccontexto;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.Intent;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private Button regresar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+
+        int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if(status == ConnectionResult.SUCCESS) {
+            // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+        }else{
+            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
+            dialog.show();
+        }
+
+        regresar = (Button)findViewById(R.id.btn_activity_regresar);
+        regresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intento = new Intent(MapsActivity.this, MainActivity.class);
+                startActivity(intento);
+            }
+        });
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mMap.setMyLocationEnabled(true);
+        UiSettings ui = mMap.getUiSettings();
+        ui.setCompassEnabled(true);
+
+        mMap.setPadding(100, 700,0,0);
+    }
+}
